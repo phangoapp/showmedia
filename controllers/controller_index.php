3c416ef4eb270e6ca97375b14859b8888878b3ae
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-04-22 01:59:17 +0200

    Fixes in showmedia

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -12,29 +12,29 @@ use PhangoApp\PhaRouter\Controller;
 Utils::load_config('config');
 Utils::load_config('config_views');
 
-//Set the theme folder media folder_env
-
-View::$media_env=array(View::$folder_env[0]);
-
-//Here don't worry about check ten directories for find view.
-
-foreach(Routes::$apps as $app)
-{
-
-	View::$folder_env[]='vendor/'.$app.'/views';
-	View::$media_env[]=View::$folder_env[0].'/'.$app;
-	View::$media_env[]='vendor/'.$app;
-	
-}
-
-
 class IndexController extends Controller {
 
     //http://localhost/tech/showmedia.php/css/font-awesome.min.css
 
-    public function home($type, $css)
+    public function home($type, $media_path, $media_folder='')
     {
+        
+        //Set the theme folder media folder_env
 
+        View::$media_env=array(View::$folder_env[0]);
+
+        //Here don't worry about check ten directories for find view.
+        
+        foreach(Routes::$apps as $app)
+        {
+
+            View::$folder_env[]='vendor/'.$app.'/views';
+            View::$media_env[]=View::$folder_env[0].'/'.$app.'/views';
+            View::$media_env[]='vendor/'.$app;
+            
+        }
+
+        
         View::load_media_file($_SERVER['REQUEST_URI']);
         
     }
--- urls.php
+++ urls.php
@@ -5,5 +5,6 @@ use PhangoApp\PhaRouter\Routes;
 //Routes::$urls['welcome\/([0-9]+)\/(\w+)']=array('index', 'page');
 
 Routes::$urls['showmedia\/([^\/]*)\/([^\/]*)']=array('index', 'home');
+Routes::$urls['showmedia\/([^\/]*)\/([^\/]*)\/([^\/]*)']=array('index', 'home');
 
 ?>
