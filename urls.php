134aa3c68306de8332afaec0336f7c82fd35a330
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-04-22 02:13:40 +0200

    Little fix in urls

--- urls.php
+++ urls.php
@@ -4,7 +4,7 @@ use PhangoApp\PhaRouter\Routes;
 
 //Routes::$urls['welcome\/([0-9]+)\/(\w+)']=array('index', 'page');
 
-Routes::$urls['showmedia\/([^\/]*)\/([^\/]*)']=array('index', 'home');
-Routes::$urls['showmedia\/([^\/]*)\/([^\/]*)\/([^\/]*)']=array('index', 'home');
+Routes::$urls['showmedia\/([^\/]*)\/(.*)']=array('index', 'home');
+//Routes::$urls['showmedia\/([^\/]*)\/([^\/]*)\/([^\/]*)']=array('index', 'home');
 
 ?>
